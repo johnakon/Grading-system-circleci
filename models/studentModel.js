f48449627f09c6ae9b53111037a9b8072677702e
John <john.akonya@gmail.com>
2020-02-24 15:39:39 +0300

    student model created

---
+++ models/studentModel.js
@@ -0,0 +1,9 @@
+class Student {
+  constructor(firstName, secondName, status, age) {
+    this.firstName = firstName;
+    this.secondName = secondName;
+    this.status = status;
+    this.age = age;
+  }
+}
+
