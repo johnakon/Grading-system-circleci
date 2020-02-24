be41722f6af07ef90e8a10a9953ef974b2119b0a
John <john.akonya@gmail.com>
2020-02-24 16:00:58 +0300

    grading controller

--- models/marksModel.js
+++
@@ -1,9 +0,0 @@
-class Marks {
-  constructor(student, subject, marks) {
-    this.student = student;
-    this.subject = subject;
-    this.marks = marks;
-  }
-}
-
-const score = () => {};
--- models/studentModel.js
+++
@@ -1,9 +0,0 @@
-class Student {
-  constructor(firstName, secondName, status, age) {
-    this.firstName = firstName;
-    this.secondName = secondName;
-    this.status = status;
-    this.age = age;
-  }
-}
-
